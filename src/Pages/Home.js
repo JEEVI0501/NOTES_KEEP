46a582672bafe1c1d1290a8902ae4d6c4798c253
JEEVI0501 <jijiradha29@gmail.com>
2023-03-13 00:02:01 +0530

    smells

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -17,7 +17,7 @@ export default function Home() {
     
     const auth = getAuth();
     const [note, setNote] = useState([{id:"initial"}]);
-    var noteStar=[];
+    let noteStar=[];
     useEffect(() => 
        onSnapshot(collection(db,auth.currentUser.email),(snapshot) =>
         setNote(snapshot.docs.map((doc) => ({ ...doc.data(),id:doc.id})))
